321de3e193d04682e492e4f81f0ea0c09c22d568
Sahar <sahar.bit@gmail.com>
2019-12-05 14:23:11 +0200

    Auth module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as Logger from './logger'
 import * as Analytics from './analytics'
+import Auth from './authentication'
 
-export default { Logger, Analytics }
+export default { Logger, Analytics, Auth }
